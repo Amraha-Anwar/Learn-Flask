0bfc386befa8ae84feb628e913e4bcf523b8b7fa
Amraha-Anwar <amrahaanwar@gmail.com>
2025-07-18 18:06:28 +0500

    routing

--- 01_fundamentals/main.py
+++ 01_fundamentals/main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
+def home():
     return "Heelllow from Flask <3"
+
+# routing
+@app.route("/about")
+def about():
+    return "you are on about page.."
+
+@app.route("/contact")
+def contact():
+    return "you are on contact page.."
+
+# how to use GET/POST methods (syntax)
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if request.method == "POST":
+        return "You sent data!"
+    else:
+        return "You are only viewing the form!"
